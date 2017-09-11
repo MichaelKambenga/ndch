15221dc5ff2bfad768b47ba1ee9d42ccfd48e7db
Andrew Augustino <andre@DESKTOP-0SGOF8Q.mof.go.tz>
2017-09-12 00:28:17 +0300

    Add asset file

--- assets/LteAsset.php
+++ assets/LteAsset.php
@@ -19,7 +19,7 @@ class LteAsset extends AssetBundle {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/bootstrap.min.css',
+//        'css/bootstrap.min.css',
 //        'css/font-awesome.min.css',
 //        'css/ionicons.min.css',
         'css/AdminLTE.min.css',
@@ -30,7 +30,7 @@ class LteAsset extends AssetBundle {
         'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
     ];
     public $js = [
-        'js/bootstrap/bootstrap.min.js',
+//        'js/bootstrap/bootstrap.min.js',
         'plugins/jQueryUI/jquery-ui.min.js',
         'js/raphael.min.js',
         'plugins/sparkline/jquery.sparkline.min.js',
